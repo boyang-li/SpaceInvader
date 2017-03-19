cbeccdb5312ffd983734e3e50b1a13b668c968c7
Aayani Simon <aayanisi@teach.cs.toronto.edu>
2017-03-19 14:31:36 -0400

    Commited all the base files

---
+++ SpaceInvader.v
@@ -0,0 +1,111 @@
+// Part 2 skeleton
+
+module part2
+	(
+		CLOCK_50,						//	On Board 50 MHz
+		// Your inputs and outputs here
+        KEY,
+        SW,
+		// The ports below are for the VGA output.  Do not change.
+		VGA_CLK,   						//	VGA Clock
+		VGA_HS,							//	VGA H_SYNC
+		VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,						//	VGA BLANK
+		VGA_SYNC_N,						//	VGA SYNC
+		VGA_R,   						//	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B   						//	VGA Blue[9:0]
+	);
+
+	input			CLOCK_50;				//	50 MHz
+	input   [9:0]   SW;
+	input   [3:0]   KEY;
+
+	// Declare your inputs and outputs here
+	// Do not change the following outputs
+	output			VGA_CLK;   				//	VGA Clock
+	output			VGA_HS;					//	VGA H_SYNC
+	output			VGA_VS;					//	VGA V_SYNC
+	output			VGA_BLANK_N;				//	VGA BLANK
+	output			VGA_SYNC_N;				//	VGA SYNC
+	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+	
+	wire resetn;
+	assign resetn = KEY[0];
+	
+	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+	wire [2:0] colour;
+	wire [7:0] x;
+	wire [6:0] y;
+	wire writeEn;
+
+	// Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(resetn),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+			
+	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
+	// for the VGA controller, in addition to any other functionality your design may require.
+    wire  ld_x_wire, ld_y_wire, ld_colour_wire;
+    // Instansiate datapath
+	datapath d0(
+	  //Input
+	  .data_in(SW[6:0]),
+	  .colour_in(SW[9:7]),
+	  .resetn(KEY[0]),
+	  .ld_x(ld_x_wire),
+	  .ld_y(ld_y_wire),
+	  .ld_colour(ld_colour_wire),
+	  
+	  //Output
+	  .colour(colour),
+	  .X(x),
+	  .Y(y)
+	);
+   
+    // Instansiate FSM control
+      control c0(
+	//Input
+	.clk(CLOCK_50),
+	.resetn(KEY[0]),
+	.go(KEY[1]),
+	.load(KEY[3]),
+	
+	//Output
+	.ld_x(ld_x_wire),
+	.ld_y(ld_y_wire),
+	.ld_colour(ld_colour_wire),
+	.wren(writeEn)
+      );
+    
+    
+endmodule
+
+module player(
+
+
+);
+
+
+endmodule 
